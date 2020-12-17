1e058430b062d695843891b5785a5b9b8918a5e3
Souka1987 <soukainataattoumani@yahoo.fr>
2020-12-17 17:24:21 +0100

    17/12

--- api/controllers/productsController.js
+++ api/controllers/productsController.js
@@ -22,32 +22,32 @@ module.exports = {
         res.json(dbProduct);
     },
 
-    // put: async (req, res, next) => {
-    //     const dbProduct = await Product.findById(req.params.id);
+    put: async (req, res, next) => {
+        const dbProduct = await Product.findById(req.params.id);
 
-    //     Product.findOneAndUpdate({
-    //             _id: req.params.id
-    //         }, {
-    //             title: req.body.name,
-    //         },
-    //         (err) => {
-    //             if (err) res.send(err);
-    //             else {
-    //                 console.log("Modification OK");
-    //                 res.json(dbProduct);
-    //             }
-    //         }
-    //     );
-    // },
-    // deleteOne: async (req, res, next) => {
-    //     const dbProduct = await Product.find({});
-    //     Product.deleteOne({
-    //             _id: req.params.id,
-    //         },
-    //         (err) => {
-    //             if (!err) res.json(dbProduct);
-    //             else res.send(err);
-    //         }
-    //     );
-    // },
+        Product.findOneAndUpdate({
+                _id: req.params.id
+            }, {
+                title: req.body.name,
+            },
+            (err) => {
+                if (err) res.send(err);
+                else {
+                    console.log("Modification OK");
+                    res.json(dbProduct);
+                }
+            }
+        );
+    },
+    deleteOne: async (req, res, next) => {
+        const dbProduct = await Product.find({});
+        Product.deleteOne({
+                _id: req.params.id,
+            },
+            (err) => {
+                if (!err) res.json(dbProduct);
+                else res.send(err);
+            }
+        );
+    },
 };
--- api/router.js
+++ api/router.js
@@ -25,9 +25,9 @@ router.route('/products')
   .get(productsController.get)
   .post( productsController.post)
 
-// router.route('/products/:id')
-//   .put(productsController.put)
-//   .delete(productsController.deleteOne)
+router.route('/products/:id')
+  .put(productsController.put)
+  .delete(productsController.deleteOne)
 
 
 
